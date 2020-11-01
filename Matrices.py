a133027cde97b5537768ad23d46b1ced89bcdeea
SPARTACUS5329 <58896813+SPARTACUS5329@users.noreply.github.com>
2020-11-01 13:50:56 +0530

    Refined version

--- Matrices.py
+++ Matrices.py
@@ -19,7 +19,6 @@ def determinant(matrix):
         det = 0
         for i in range(len(matrix)):
             subMatrix = [[] for alpha in range(len(matrix[0])-1)]
-            subMatrixElementSelectionKey = 0
             topElement = matrix[0][i]
             for j in range(1,len(matrix)):
                 for k in range(len(matrix)):
@@ -27,15 +26,12 @@ def determinant(matrix):
                         continue
                     else:
                         subMatrix[j-1].append(matrix[j][k])
-                        subMatrixElementSelectionKey+=1
-            det+=(-1)**(i)*matrix[0][i]*determinant(subMatrix)
+            det+=(-1)**(i)*topElement*determinant(subMatrix)
         return det
                 
-
-N = int(input("Enter N for NxN matrix: "))
-matrix = [[int(i) for i in input().split()] for j in range(N)]
-print(N)
-
-displayMatrix(transpose(matrix))
-print(determinant(matrix))
-print(determinant(matrix)-determinant(transpose(matrix)))
+if __name__ == "__main__":   
+    N = int(input("Enter N for NxN matrix: "))
+    matrix = [[int(i) for i in input().split()] for j in range(N)]
+    displayMatrix(transpose(matrix))
+    print(f"{determinant(matrix)} is the determinant of this matrix")
+    print(f"{determinant(matrix)-determinant(transpose(matrix))} is a simple check to see the determinant works. Because det(A) = det(transpose(A))")
