7e822125e94ccbe95adbefe7c63d31f7b604b4ff
SPARTACUS5329 <58896813+SPARTACUS5329@users.noreply.github.com>
2020-11-01 17:52:52 +0530

    Made Matrix object

--- Matrices.py
+++ Matrices.py
@@ -1,47 +1,66 @@
-def displayMatrix(matrix):
-    for i in matrix:
-        for j in i:
-            print(j,end = ' ')
-        print()
-    return matrix
+class Matrix:
+    def __init__(self, N):
+        self.N = self.order = self.dimensions = N
+        self.matrix = self.constructMatrix()
 
-def transpose(matrix):
-    for i in range(len(matrix)):
-        #Iterating throught the matrix by increasing column number every time to not make the net result 0 change
-        for j in range(i, len(matrix)):
-            #Swapping the matrix elements in transpose conditions
-            matrix[i][j],matrix[j][i] = matrix[j][i],matrix[i][j]
-    return matrix
-
-def determinant(matrix):
-    #Reducing the N order matrix to 2x2s and evaluating them
-    #Alternatively they can be converted to 1x1s and they don't need to be evaulated, but time complexity might be more in some cases
-    #Like this:
-    # if len(matrix) == 1:
-    #     return matrix[0][0]
-    if len(matrix)==2:
-        return matrix[1][1]*matrix[0][0] - matrix[1][0]*matrix[0][1]
+    def __str__(self):
+        self.displayMatrix()
+        return ''
     
-    else:
-        det = 0
-        for i in range(len(matrix)):
-            subMatrix = [[] for alpha in range(len(matrix[0])-1)]
-            topElement = matrix[0][i]
-            #j starts from 1 to exclude the first row in the determinant calculation
-            for j in range(1,len(matrix)):
-                for k in range(len(matrix)):
-                    #k==i is excluded for subMatrix calculation
-                    if k==i:
-                        continue
-                    else:
-                        subMatrix[j-1].append(matrix[j][k])
-            #cofactor is calculated
-            det+=(-1)**(i)*topElement*determinant(subMatrix)
-        return det
-                
+    def constructMatrix(self):
+        return [[int(i) for i in input().split()] for j in range(self.N)]
+
+    def displayMatrix(self):
+        for i in self.matrix:
+            for j in i:
+                print(j,end = ' ')
+            print()
+        return self.matrix
+
+    def isSymmetric(self):
+        return self.matrix == self.transpose()
+
+    def transpose(self):
+        for i in range(len(self.matrix)):
+            #Iterating throught the matrix by increasing column number every time to not make the net result 0 change
+            for j in range(i, len(self.matrix)):
+                #Swapping the matrix elements in transpose conditions
+                self.matrix[i][j],self.matrix[j][i] = self.matrix[j][i],self.matrix[i][j]
+        return self.matrix
+
+    def determinant(self):
+        #Reducing the N order matrix to 2x2s and evaluating them
+        #Alternatively they can be converted to 1x1s and they don't need to be evaulated, but time complexity might be more in some cases
+        #Like this:
+        # if len(matrix) == 1:
+        #     return matrix[0][0]
+        if len(self.matrix)==2:
+            return self.matrix[1][1]*self.matrix[0][0] - self.matrix[1][0]*self.matrix[0][1]
+        
+        else:
+            self.det = 0
+            for i in range(len(self.matrix)):
+                self.subMatrix = SubMatrix(len(self.matrix[0])-1)
+                topElement = self.matrix[0][i]
+                #j starts from 1 to exclude the first row in the determinant calculation
+                for j in range(1,len(self.matrix)):
+                    for k in range(len(self.matrix)):
+                        #k==i is excluded for subMatrix calculation
+                        if k==i:
+                            continue
+                        else:
+                            self.subMatrix.matrix[j-1].append(self.matrix[j][k])
+                #cofactor is calculated
+                self.det+=(-1)**(i)*topElement*self.subMatrix.determinant()
+            return self.det
+
+#Creating a class SubMatrix that has all methods of Matrix but the form of creation is different. This one unlike Matrix is not user defined.
+class SubMatrix(Matrix):
+    def __init__(self, N):
+        self.matrix = [[] for alpha in range(N)]
+
 if __name__ == "__main__":   
     N = int(input("Enter N for NxN matrix: "))
-    matrix = [[int(i) for i in input().split()] for j in range(N)]
-    displayMatrix(transpose(matrix))
-    print(f"{determinant(matrix)} is the determinant of this matrix")
-    print(f"{determinant(matrix)-determinant(transpose(matrix))} is a simple check to see the determinant works. Because det(A) = det(transpose(A))")
+    matrix = Matrix(N)
+    print(f"{matrix.determinant()} is the determinant of this matrix")
+    print(f"{matrix.determinant()-matrix.transpose().determinant()} is a simple check to see the determinant works. Because det(A) = det(transpose(A))")
